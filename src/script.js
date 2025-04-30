ee1da22e4987ebe2ff330c0439159fac17317441
nhuthoangdon <50766646+nhuthoangdon@users.noreply.github.com>
2025-04-30 13:25:54 +0700

    Update license, about pages

--- src/script.js
+++ src/script.js
@@ -1,13 +1,3 @@
 // pages scripts, excludes Home
 
-document.addEventListener('DOMContentLoaded', () => {
-    const currentPage = window.location.pathname.split('/').pop() || 'index.html'; // Get current page name
-    const menuLinks = document.querySelectorAll('.menu a');
 
-    menuLinks.forEach(link => {
-        const href = link.getAttribute('href').split('/').pop(); // Get link's target page
-        if (href === currentPage) {
-            link.classList.add('current');
-        }
-    });
-});
